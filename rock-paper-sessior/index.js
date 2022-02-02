4d24c80a8f292702990f2f2b4e1a1e77848d57d4
vivek reddy Kasi Reddy <>
2022-02-02 08:53:21 -0600

    first commit

---
+++ rock-paper-sessior/index.js
@@ -0,0 +1,171 @@
+//initial player and computer score 
+let playerInitalScore = 0;
+let computerInitalScore = 0;
+
+// initial player number 
+let playerNumber;
+let computerNumber;
+
+// game status text 
+let gameStatusText = "";
+let gameStatus="";
+
+
+// get player score 
+let playerScore = document.getElementById("playerScore");
+let computerScore = document.getElementById("computerScore");
+
+// get buttons with id 
+let rockButton = document.getElementById("RockClickButton");
+let paperButton = document.getElementById("paperClickButton");
+let sessiorButton = document.getElementById("sessiorClickButton");
+
+
+// output elements 
+let outputTextContainer = document.getElementById("outputTextContainer");
+let computerChoice = document.getElementById("computerChoice");
+let playerChoice = document.getElementById("playerChoice");
+let resultText = document.getElementById("resultText");
+
+
+// computer choice text 
+let computerChoiceText = "";
+let playerChoiceText = "";
+
+
+
+//display initial score of player and computer by calling function updateScore();
+function updateScore(){
+    playerScore.textContent = playerInitalScore;
+    computerScore.textContent = computerInitalScore;
+}
+updateScore();
+
+
+
+
+function displayText(){
+    outputTextContainer.style.display="block";
+    //computer choice text 
+    if(computerChoiceText==="Rock"){
+        computerChoice.style.color="#FF33CE";
+    }else if (computerChoiceText==="Paper"){
+        computerChoice.style.color="#F8F004";
+    }
+    else if(computerChoiceText==="Scissors"){
+        computerChoice.style.color = "#0499F8";
+    } 
+    computerChoice.textContent = computerChoiceText;
+    // player choice text
+    if(playerChoiceText==="Rock"){
+        playerChoice.style.color="#FF33CE";
+    }else if (playerChoiceText==="Paper"){
+        playerChoice.style.color="#F8F004";
+    }
+    else if(playerChoiceText==="Scissors"){
+        playerChoice.style.color = "#0499F8";
+    }
+    //game status text
+    playerChoice.textContent = playerChoiceText;
+    if(gameStatus === "tie"){
+        resultText.style.color="Brown";
+    }
+    else if(gameStatus==="won"){
+        resultText.style.color="Green";
+    }
+    else{
+        resultText.style.color="red";
+    }
+    resultText.textContent = gameStatusText;
+}
+
+
+// dont display output text initially 
+outputTextContainer.style.display="none";
+// assigning number to rock paper and sessior 
+// rock is 1   
+// paper is 2 
+// sessior is 3
+
+
+// onclick rock button function 
+rockButton.onclick = ()=>{
+    outputTextContainer.style.display="none";
+    playerNumber = 1;
+    playerChoiceText = "Rock";
+    computerNumber = Math.floor(Math.random()*3)+1; // this random function generates only 1 or 2 or 3 
+    if(playerNumber===computerNumber){
+        computerChoiceText = "Rock";
+        gameStatusText = "Game is a Tie !!"
+        gameStatus = "tie";
+    }
+    else if (playerNumber===1 && computerNumber==2){
+        computerChoiceText = "Paper";
+        gameStatusText = "Computer Won !!"
+        gameStatus = "lose";
+        computerInitalScore=computerInitalScore+1;
+    }
+    else if(playerNumber===1 && computerNumber===3){
+        computerChoiceText = "Scissors";
+        gameStatusText = "Player Won !!";
+        gameStatus = "won";
+        playerInitalScore = playerInitalScore + 1;
+    }
+    displayText();
+    updateScore();
+}
+
+// onclick paper button function 
+paperButton.onclick = ()=>{
+    outputTextContainer.style.display="none";
+    playerNumber = 2;
+    playerChoiceText = "Paper";
+    computerNumber = Math.floor(Math.random()*3)+1; // this random function generates only 1 or 2 or 3 
+    if(playerNumber===computerNumber){
+        computerChoiceText = "Paper";
+        gameStatusText = "Game is a Tie !!";
+        gameStatus = "tie";
+    }
+    else if (playerNumber===2 && computerNumber==3){
+        computerChoiceText = "Scissors";
+        gameStatusText = "Computer Won !!";
+        gameStatus = "lose";
+        computerInitalScore=computerInitalScore+1;
+    }
+    else if(playerNumber===2 && computerNumber===1){
+        computerChoiceText = "Rock";
+        gameStatusText = "Player Won !!";
+        gameStatus = "won";
+        playerInitalScore = playerInitalScore + 1;
+    }
+    displayText();
+    updateScore();
+}
+
+
+// onclick sessior button function 
+sessiorButton.onclick = ()=>{
+    outputTextContainer.style.display="none";
+    playerNumber = 3;
+    playerChoiceText = "Scissors";
+    computerNumber = Math.floor(Math.random()*3)+1; // this random function generates only 1 or 2 or 3 
+    if(playerNumber===computerNumber){
+        computerChoiceText = "Scissors";
+        gameStatusText = "Game is a Tie !!";
+        gameStatus = "tie";
+    }
+    else if (playerNumber===3 && computerNumber===1){
+        computerChoiceText = "Rock";
+        gameStatusText = "Computer Won !!";
+        gameStatus = "lose";
+        computerInitalScore=computerInitalScore+1;
+    }
+    else if(playerNumber===3 && computerNumber===2){
+        computerChoiceText = "Paper";
+        gameStatusText = "Player Won !!";
+        gameStatus = "won";
+        playerInitalScore = playerInitalScore + 1;
+    }
+    displayText();
+    updateScore();
+}
